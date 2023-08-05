a7964796d8d03612b267440082aed41dd8de1748
EastOrb <102253515+EastOrb@users.noreply.github.com>
2023-08-06 00:42:50 +0100

    Update index.ts
    Added a helper function to check if a vehicle matches certain criteria and a sorting function to personalise search of vehicles based on certain properties

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
-// cannister code goes here
-import { $query, $update, Record, StableBTreeMap, Vec, match, Result, nat64, ic, Opt } from 'azle';
+import { $query, Record, StableBTreeMap, Result, nat64, ic } from 'azle';
 import { v4 as uuidv4 } from 'uuid';
 
 type Vehicle = Record<{
@@ -19,45 +18,75 @@ type DepreciationPayload = Record<{
     depreciation: nat64;
 }>
 
-const vehicleStorage = new StableBTreeMap<string, Vehicle>(0, 44, 1024);
+const vehicleDataStorage = new StableBTreeMap<string, Vehicle>(0, 44, 1024);
 
 $query;
 export function getVehicle(id: string): Result<Vehicle, string> {
-    return match(vehicleStorage.get(id), {
-        Some: (Vehicle) => Result.Ok<Vehicle, string>(Vehicle),
-        None: () => Result.Err<Vehicle, string>(`a vehicle with id=${id} not found`)
+    return match(vehicleDataStorage.get(id), {
+        Some: (vehicle) => Result.Ok<Vehicle, string>(vehicle),
+        None: () => Result.Err<Vehicle, string>(`A vehicle with id=${id} not found`)
     });
 }
 
-$update;
 export function addMileage(id: string, payload: MileagePayload): Result<Vehicle, string> {
-    return match(vehicleStorage.get(id), {
-        Some: (Vehicle) => {
-            const updatedVehicle: Vehicle = {
-                ...Vehicle,
-                mileage: Vehicle.mileage + payload.miles,
-                createdAt: ic.time()
-            };
-            vehicleStorage.insert(Vehicle.id, updatedVehicle);
-            return Result.Ok<Vehicle, string>(updatedVehicle);
-        },
-        None: () => Result.Err<Vehicle, string>(`a vehicle with id=${id} not found`)
-    });
+    const vehicle = vehicleDataStorage.get(id);
+    if (!vehicle) {
+        return Result.Err<Vehicle, string>(`A vehicle with id=${id} not found`);
+    }
+
+    const updatedVehicle: Vehicle = {
+        ...vehicle,
+        mileage: vehicle.mileage + payload.miles,
+        createdAt: ic.time()
+    };
+    vehicleDataStorage.insert(vehicle.id, updatedVehicle);
+
+    return Result.Ok<Vehicle, string>(updatedVehicle);
 }
 
-
-$update;
 export function calculateDepreciation(id: string, payload: DepreciationPayload): Result<Vehicle, string> {
-    return match(vehicleStorage.get(id), {
-        Some: (Vehicle) => {
-            const updatedVehicle: Vehicle = {
-                ...Vehicle,
-                depreciation: payload.depreciation,
-                createdAt: ic.time()
-            };
-            vehicleStorage.insert(Vehicle.id, updatedVehicle);
-            return Result.Ok<Vehicle, string>(updatedVehicle);
-        },
-        None: () => Result.Err<Vehicle, string>(`a vehicle with id=${id} not found`)
-    });
+    const vehicle = vehicleDataStorage.get(id);
+    if (!vehicle) {
+        return Result.Err<Vehicle, string>(`A vehicle with id=${id} not found`);
+    }
+
+    const adjustedDepreciation = vehicle.depreciation + payload.depreciation;
+    const updatedVehicle: Vehicle = {
+        ...vehicle,
+        depreciation: adjustedDepreciation,
+        createdAt: ic.time()
+    };
+    vehicleDataStorage.insert(vehicle.id, updatedVehicle);
+
+    return Result.Ok<Vehicle, string>(updatedVehicle);
+}
+
+// Retrieve vehicles that match the given criteria
+export function filterVehicles(criteria: Partial<Vehicle>): Vehicle[] {
+    const vehicles: Vehicle[] = [];
+    for (const [, vehicle] of vehicleDataStorage.entries()) {
+        if (isVehicleMatchingCriteria(vehicle, criteria)) {
+            vehicles.push(vehicle);
+        }
+    }
+    return vehicles;
+}
+
+// Helper function to check if a vehicle matches the given criteria
+function isVehicleMatchingCriteria(vehicle: Vehicle, criteria: Partial<Vehicle>): boolean {
+    for (const key in criteria) {
+        if (vehicle[key] !== criteria[key]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Sort vehicles based on the given property
+export function sortVehicles(property: keyof Vehicle): Vehicle[] {
+    const vehicles: Vehicle[] = [];
+    for (const [, vehicle] of vehicleDataStorage.entries()) {
+        vehicles.push(vehicle);
+    }
+    return vehicles.sort((a, b) => a[property] - b[property]);
 }
